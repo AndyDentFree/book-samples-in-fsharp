679dc2ddff85da3a9890fe06192a280469638e82
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2008-09-15 09:56:41 +0000

    Continue with chapter 8.

--- trunk/TheLittleMler-Felleisen_Friedman/8_BowsAndArrows.fs
+++ trunk/TheLittleMler-Felleisen_Friedman/8_BowsAndArrows.fs
@@ -28,9 +28,30 @@ let rec subst rel n a = function
             then Cons(n,subst rel n a t)
             else Cons(e,subst rel n a t)
 
+let inRange ((small, large), x) =
+    if small < x
+        then x < large
+        else false
+
 let rec substPred pred n = function
     | Empty     -> Empty
     | Cons(e,t) ->
         if pred e
             then Cons(n,substPred pred n t)
             else Cons(e,substPred pred n t)
+
+let is15 n =
+    n = 15
+
+let lessThan15 x =
+    x < 15
+
+let inRange11_16 x =
+    if 11 < x
+        then x < 16
+        else false
+
+let inRangeC (small, large) x =
+    if small < x
+        then x < large
+        else false

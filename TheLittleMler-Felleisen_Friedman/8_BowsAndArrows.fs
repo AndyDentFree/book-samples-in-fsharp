9545204c576c9581ff074c7f3d84da8db69a99f7
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2008-09-16 02:15:19 +0000

    Continue with Chapter 8.

--- TheLittleMler-Felleisen_Friedman/8_BowsAndArrows.fs
+++ TheLittleMler-Felleisen_Friedman/8_BowsAndArrows.fs
@@ -23,7 +23,7 @@ let rec substOrapl = function
 let rec subst = function
     | rel,n,a,Empty     -> Empty
     | rel,n,a,Cons(e,t) ->
-        if rel a e
+        if rel (a,e)
             then Cons(n,subst (rel, n, a, t))
             else Cons(e,subst (rel, n, a, t))
 
@@ -50,7 +50,49 @@ let inRange11_16 x =
         then x < 16
         else false
 
-let inRangeC (small, large) x =
-    if small < x
-        then x < large
+let inRangeC (small, large) =
+    let inRange' x =
+        if small < x
+            then x < large
+            else false
+    inRange'
+
+let inRangeC11_16 x =
+    if 11 < x
+        then x < 16
         else false
+
+let rec substC pred =
+    let substC' = function
+    | n,Empty     -> Empty
+    | n,Cons(e,t) ->
+        if pred e
+            then Cons(n,substC pred (n,t))
+            else Cons(e,substC pred (n,t))
+    substC'
+
+let rec substCInRange11_16 = function
+    | n,Empty     -> Empty
+    | n,Cons(e,t) ->
+        if inRange11_16 e
+            then Cons(n,substCInRange11_16 (n,t))
+            else Cons(e,substCInRange11_16 (n,t))
+
+let rec combine = function
+    | Empty,l2      -> l2
+    | Cons(a,l1),l2 ->
+        Cons(a,combine (l1,l2))
+
+let rec combineC x =
+    let combineC' l2 =
+        match x with
+        | Empty      -> l2
+        | Cons(a,l1) ->
+            Cons(a,combineC l1 l2)
+    combineC'
+
+let prefixer123 l2 =
+    Cons(1,
+        Cons(2,
+            Cons(3,
+                l2)))

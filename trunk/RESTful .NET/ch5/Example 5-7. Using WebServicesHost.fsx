695fa726d437e3d73ee8b710e688c232ad85fb07
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2008-12-10 13:00:03 +0000

    Add Example 5-7.

---
+++ trunk/RESTful .NET/ch5/Example 5-7. Using WebServicesHost.fsx
@@ -0,0 +1,46 @@
+#light
+#r "System.ServiceModel"
+#r "System.ServiceModel.Web"
+open System
+open System.ServiceModel
+open System.ServiceModel.Web
+open System.ServiceModel.Description
+
+
+[<ServiceContract>]
+type HostingExample() =
+    [<WebGet(UriTemplate = "*")>]
+    [<OperationContract>]
+    member this.Method1() =
+        "String Result"
+
+
+let baseAddress = [| new Uri("http://localhost:8080/Hosting") |]
+let sh = new WebServiceHost(typeof<HostingExample>, baseAddress)
+
+let mutable openSucceeded = false
+try
+    try
+        sh.Open()
+        openSucceeded <- true
+    with ex ->
+        printfn "ServiceHost failed to open %s" (ex.ToString())
+finally
+    if not openSucceeded then sh.Abort()
+
+if openSucceeded then
+    printfn "Service is running..."
+    Console.ReadLine() |> ignore
+    
+    // Robust Close Example
+    let mutable closeSucceeded = false
+    try 
+        try
+            sh.Close()
+            closeSucceeded <- true
+        with ex ->
+            printfn "ServiceHost failed to close %s" (ex.ToString())
+    finally
+        if not closeSucceeded then sh.Abort()
+else
+    printfn "Service failed to open"

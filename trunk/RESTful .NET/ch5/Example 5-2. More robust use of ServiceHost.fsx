771d8b2160699fe79f063b90fbb198bdbbfbaa65
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2008-12-10 03:11:56 +0000

    Add chapter 5, example 5-2.

---
+++ trunk/RESTful .NET/ch5/Example 5-2. More robust use of ServiceHost.fsx
@@ -0,0 +1,42 @@
+#light
+#r "System.ServiceModel"
+#r "System.ServiceModel.Web"
+open System
+open System.ServiceModel
+open System.ServiceModel.Web
+open System.ServiceModel.Description
+
+
+[<ServiceContract>]
+type HostingExample() =
+    [<WebGet(UriTemplate = "*")>]
+    [<OperationContract>]
+    member this.Method1() =
+        "String Result"
+
+
+let baseAddresses : Uri[] = [| |]
+let sh = new ServiceHost(typeof<HostingExample>, baseAddresses)
+// flag to check if call to Open succeeded
+let mutable openSucceeded = false
+
+try
+    let se = sh.AddServiceEndpoint(typeof<HostingExample>,
+                                   new WebHttpBinding(), 
+                                   "http://localhost:8080/Hosting")
+    se.Behaviors.Add(new WebHttpBehavior())
+    try
+        sh.Open()
+        openSucceeded <- true
+    with ex ->
+        printfn "ServiceHost failed to open %s" (ex.ToString())
+finally
+    // call Abort since the object will be in the Faulted state
+    if not openSucceeded then sh.Abort()
+
+if openSucceeded then
+    printfn "Service is running..."
+    Console.ReadLine() |> ignore
+    sh.Close()
+else
+    printfn "Service failed to open"

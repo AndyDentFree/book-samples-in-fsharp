ae38f2b0971c87f71a8eda686d23304b67bb6afb
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2008-12-14 21:08:55 +0000

    Add Programming WCF Services. Add Example 1-11. Enabling the metadata exchange behavior programmatically. Add Example 1-9. Service-side programmatic endpoint configuration.

---
+++ trunk/Programming WCF Services/1-WCF_Essentials/Example 1-9. Service-side programmatic endpoint configuration.fsx
@@ -0,0 +1,33 @@
+#light
+#r "System.ServiceModel"
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+
+
+[<ServiceContract>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyMethod : unit -> unit
+
+
+type MyService() =
+    interface IMyContract with
+        member this.MyMethod() =
+            printfn "MyService.MyMethod()"
+
+
+let baseAddresses : Uri[] = [| |]
+let host = new ServiceHost(typeof<MyService>, baseAddresses)
+
+let wsBinding = new WSHttpBinding()
+let tcpBinding = new NetTcpBinding()
+
+host.AddServiceEndpoint(typeof<IMyContract>, wsBinding, "http://localhost:8000/MyService")
+host.AddServiceEndpoint(typeof<IMyContract>, tcpBinding, "net.tcp://localhost:8001/MyService")
+host.AddServiceEndpoint(typeof<IMyContract>, tcpBinding, "net.tcp://localhost:8002/MyService")
+
+host.Open()
+printfn "Service Opened...\nPress any key to end..."
+Console.ReadKey(true) |> ignore
+host.Close()

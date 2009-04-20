8150f06db9b315de762709c1a02a3632634449b2
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-04-20 02:14:34 +0000

    Rename ch1 to ch01. Add ch03 and BookStore.

---
+++ Real World Haskell/ch03/BookStore.fsx
@@ -0,0 +1,22 @@
+// file: ch03/BookStore.fsx
+#light
+
+type BookInfo = Book of int64 * string * string list
+
+let myInfo = Book(9780135072455L, "Algebra of Programming", ["Oege de Moor"])
+
+type CustomerID = int
+type ReviewBody = string
+
+type BookReview = BookReview of BookInfo * CustomerID * string
+
+type BetterReview = BetterReview of BookInfo * CustomerID * ReviewBody
+
+type CardHolder = string
+type CardNumber = string
+type Address = string list
+
+type BillingInfo =
+    | CreditCard of CardNumber * CardHolder * Address
+    | CashOnDelivery
+    | Invoice of CustomerID

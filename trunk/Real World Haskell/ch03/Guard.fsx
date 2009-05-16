a565536588e27c75fc07b834775ecb44679859dd
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-05-16 19:56:34 +0000

    Add more ch03 examples.

---
+++ trunk/Real World Haskell/ch03/Guard.fsx
@@ -0,0 +1,12 @@
+#light
+#use "Tree.fsx"
+
+let bad_nodesAreSame node1 node2 =
+    match node1, node2 with
+    | Node(a, _, _), Node(a, _, _) -> Some a
+    | _            , _             -> None
+
+let nodesAreSame node1 node2 =
+    match node1, node2 with
+    | Node(a, _, _), Node(b, _, _) when a = b -> Some a
+    | _            , _                        -> None
--- trunk/Real World Haskell/ch03/Tree.fsx
+++ trunk/Real World Haskell/ch03/Tree.fsx
@@ -3,3 +3,8 @@
 type 'a Tree = Node of 'a * 'a Tree * 'a Tree
              | Empty
 
+let bad_nodesAreSame node1 node2 =
+    match node1, node2 with
+    | Node(a, _, _), Node(a, _, _) -> Some a
+    | _            , _             -> None
+    

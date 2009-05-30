89d2ae1902868a29aac39623e9029ab7cdf9e31b
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-05-30 19:48:46 +0000

    Add EfficientList examples.

---
+++ trunk/Real World Haskell/ch04/EfficientList.fsx
@@ -0,0 +1,13 @@
+#light
+
+let myDumbExample xs = if List.length xs > 0
+                       then List.hd xs
+                       else 'Z'
+
+let mySmartExample xs = if not (List.isEmpty xs)
+                        then List.hd xs
+                        else 'Z'
+
+let myOtherExample = function
+    | (x::_) -> x
+    | []     -> 'Z'

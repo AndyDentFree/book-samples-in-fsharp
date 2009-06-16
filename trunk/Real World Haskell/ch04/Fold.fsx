c65ff65cd8ce763378045f3c023673ac6ef1f00a
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-06-16 00:57:37 +0000

    Add to Fold examples.

--- trunk/Real World Haskell/ch04/Fold.fsx
+++ trunk/Real World Haskell/ch04/Fold.fsx
@@ -2,7 +2,36 @@ let rec foldl step zero = function
     | x::xs -> foldl step (step zero x) xs
     | []    -> zero
 
-let step acc x = acc + x
-let foldSum xs = foldl step 0 xs
+
+let foldSum xs =
+    let step acc x = acc + x
+    foldl step 0 xs
 
 let niceSum xs = foldl (+) 0 xs
+
+let rec foldr step zero = function
+    | x::xs -> step x (foldr step zero xs)
+    | []    -> zero
+
+let rec filter p = function
+    | []             -> []
+    | x::xs when p x -> x :: filter p xs
+    | x::xs          -> filter p xs
+        
+let myFilter p xs =
+    let step x = function
+        | ys when p x -> x :: ys
+        | ys          -> ys
+    foldr step [] xs
+
+let myMap f xs =
+    let step x ys = f x :: ys
+    foldr step [] xs
+
+let myFoldl f z xs =
+    let step x g a = g (f a x)
+    foldr step id xs z
+
+let identity xs = foldr List.append [] xs
+
+let append xs ys = foldr List.append ys xs

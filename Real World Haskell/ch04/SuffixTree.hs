f4408e1cafe188f8c6bc12e7a1a1a923d8b6be13
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-06-24 02:45:15 +0000

    Add suffixes2 function.

--- Real World Haskell/ch04/SuffixTree.hs
+++ Real World Haskell/ch04/SuffixTree.hs
@@ -7,5 +7,9 @@ noAsPattern :: [a] -> [[a]]
 noAsPattern (x:xs) = (x:xs) : noAsPattern xs
 noAsPattern _ = []
 
+suffixes2 xs = init (tail xs)
+
 compose :: (b -> c) -> (a -> b) -> a -> c
 compose f g x = f (g x)
+
+

a3c5a7724572d63cda79c312124a0c3c38255e6f
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2009-06-27 02:47:32 +0000

    Add Prettify example.

---
+++ Real World Haskell/ch05/Prettify.fsx
@@ -0,0 +1,24 @@
+type Doc = Empty
+                  | Char of char
+                  | Text of string
+                  | Line
+                  | Concat of Doc * Doc
+                  | Union of Doc * Doc
+
+let empty = Empty
+
+let char c = Char c
+
+let text = function
+    | "" -> Empty
+    | s  -> Text s
+
+let double (d : double) = text (string d)
+
+let line = Line
+
+let (^^) x y =
+    match x, y with
+    | Empty, y -> y
+    | x, Empty -> x
+    | x, y     -> Concat(x, y)

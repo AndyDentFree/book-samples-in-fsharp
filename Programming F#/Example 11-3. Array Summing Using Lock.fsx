1c1eecf9a5c9b22c2b3ce5c6d74c2d63a356cb24
r.vernagus <r.vernagus@50607efa-7e0a-11dd-a943-c76e4c82ec5c>
2010-02-25 02:20:01 +0000

    Add Examples 11-1, 11-2, 11-3.

---
+++ Programming F#/Example 11-3. Array Summing Using Lock.fsx
@@ -0,0 +1,31 @@
+open System.Threading
+
+let sumArray (arr : int[]) =
+    let total = ref 0
+    
+    let thread1Finished = ref false
+    
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = 0 to arr.Length / 2 - 1 do
+                    lock (total) (fun () -> total := arr.[i] + !total)
+                 thread1Finished := true
+        ) |> ignore
+    
+    let thread2Finished = ref false
+    
+    ThreadPool.QueueUserWorkItem(
+        fun _ -> for i = arr.Length / 2 to arr.Length - 1 do
+                    lock (total) (fun () -> total := arr.[i] + !total)
+                 thread2Finished := true
+        ) |> ignore
+        
+    while !thread1Finished = false ||
+          !thread2Finished = false do
+          
+          Thread.Sleep(0)
+          
+    !total
+
+let millionOnes = Array.create 1000000 1
+
+printfn "sumArray millionOnes = %d" (sumArray millionOnes)
